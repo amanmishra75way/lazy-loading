4f323392ed5a2efa08b4d7a45ba4d5e54c8528fc
amanmishra75way <amishra.75way@gmail.com>
2025-06-25 16:16:32 +0530

    Add About page with lazy loading and routing setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { lazy, StrictMode, Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const About = lazy(() => import("./About.jsx"));
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route
+        path="/about"
+        element={
+          <Suspense fallback={<h1>Loading About...</h1>}>
+            <About />
+          </Suspense>
+        }
+      />
+    </Routes>
+  </BrowserRouter>
+);
