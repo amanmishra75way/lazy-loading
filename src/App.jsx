c174dc030bc0377a6667ba802f16d3dda2a4032e
amanmishra75way <amishra.75way@gmail.com>
2025-06-25 15:53:24 +0530

    loading for User component and initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import User from "./User";
 
 const App = () => {
-  return <div>App</div>;
+  const [load, setLoad] = useState(false);
+  return (
+    <>
+      <div>Lazy Loading</div>
+      {/* we want this componet to laod only button cick but it still loading on initials which consumes time */}
+      {load ? <User /> : ""}
+      <button onClick={() => setLoad(true)}>Load User</button>
+    </>
+  );
 };
 
 export default App;
