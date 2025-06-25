172039a72320f48df21b7ed6da46ad09e6dd4f99
amanmishra75way <amishra.75way@gmail.com>
2025-06-25 16:07:53 +0530

    Implement lazy loading for User component with suspense fallback

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import React, { useState } from "react";
-import User from "./User";
-
+import React, { lazy, Suspense, useState } from "react";
+// import User from "./User";
+const User = lazy(() => import("./User")); // Import like thsis
 const App = () => {
   const [load, setLoad] = useState(false);
   return (
     <>
       <div>Lazy Loading</div>
       {/* we want this componet to laod only button cick but it still loading on initials which consumes time */}
-      {load ? <User /> : ""}
-      <button onClick={() => setLoad(true)}>Load User</button>
+      {load ? (
+        <Suspense fallback={<h1>Loading...</h1>}>
+          <User />
+        </Suspense>
+      ) : (
+        ""
+      )}
+      <button className="border border-black " onClick={() => setLoad(true)}>
+        Load User
+      </button>
     </>
   );
 };
